84913b814995d2a5284b90fb02dcecffdf1b0be0
Bushra Hossain <bushrahossainonika@gmail.com>
2024-10-13 23:39:09 +0600

    Variables

--- HelloLearn/Program.cs
+++ HelloLearn/Program.cs
@@ -11,12 +11,33 @@ namespace HelloLearn
         static void Main(string[] args)
         {
             //Bhoo testing git
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("C# using for games,console appiications.");
-            Console.WriteLine("C# runs in .net Framework.So thats means C# is a part of .net framework");
-            Console.WriteLine(".net framework is a open source framework. Means anyone can contribute there through github");
+            // C# is a case sensetive programing language
+            /* Console.WriteLine("Hello World!");
+             Console.WriteLine("C# using for games,console appiications.");
+             Console.WriteLine("C# runs in .net Framework.So thats means C# is a part of .net framework");
+             Console.WriteLine(".net framework is a open source framework. Means anyone can contribute there through github");
+            */
+            /*  Data types in C#:
+               Integer - int age=23;
 
+               Floating points num - float num;
+               Character - char c = 'B';
+               String - string inp = "Bush"
 
+             */
+            int a = 15;
+            float b = 10.2F;  //110.2 is consider double that's why have to add F to clarification
+            char c = 'b';
+            string s = "Bushra is lazy";
+            Console.WriteLine(s);
+            Console.WriteLine(c);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+           
+
+            //Taking user input for the string input only
+            string userInput = Console.ReadLine();
+            Console.WriteLine("User input String: "+ userInput);
 
 
             Console.ReadLine();
