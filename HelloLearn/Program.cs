98d42128262f126b10ff6e28dadcddc5a293930b
Bushra Hossain <bushrahossainonika@gmail.com>
2024-10-13 20:57:01 +0600

    Done with 1st introduction part

--- HelloLearn/Program.cs
+++ HelloLearn/Program.cs
@@ -12,6 +12,12 @@ namespace HelloLearn
         {
             //Bhoo testing git
             Console.WriteLine("Hello World!");
+            Console.WriteLine("C# using for games,console appiications.");
+            Console.WriteLine("C# runs in .net Framework.So thats means C# is a part of .net framework");
+            Console.WriteLine(".net framework is a open source framework. Means anyone can contribute there through github");
+
+
+
 
             Console.ReadLine();
         }
